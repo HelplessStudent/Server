356db2d1686f75c64aa206f6b18ee2e91b0cb16f
HelplessStudent <74906645+HelplessStudent@users.noreply.github.com>
2021-12-04 22:32:52 +0100

    2.0
    Created login, but u cannot use same name in DB, cause it will make troulbes for you.
    Fix incoming in the distant future

--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,12 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.*;
+import java.util.Arrays;
 
 public class Server{
     public static int ID = 0;
@@ -18,8 +16,7 @@ public class Server{
     //private static Object Server;
 
     public static void main(String[] args)
-            throws Exception, SQLException
-    {
+            throws Exception {
         //Loading JDBC driver
         //DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
         String driver = "com.mysql.cj.jdbc.Driver";
@@ -42,29 +39,31 @@ public class Server{
                     new InputStreamReader(
                             s.getInputStream()));
 
+            //ps.println("Please type what action do u want to do: Register or Login");
+
             String line = br.readLine();
             System.out.println("Message: " + line);
-            String[] data;
-            data = new String[5];
+            String[] regg;
+            regg = new String[5];
+            String[] logg;
+            logg = new String[2];
 
-            if (line.equals("Register")) {
+            if (line.equals("Register") || line.equals("register")) {
                 while (true) {
                     ps.println("Enter your ID");
                     String ID = br.readLine();
                     System.out.println(ID);
                     if (!ID.isEmpty()) {
-                        data[0] = ID;
+                        regg[0] = ID;
                         break;
                     }
-                    String line2 = br.readLine();
-                    System.out.println(line2);
                 }
                 while (true) {
                     ps.println("Enter your Name");
                     String name = br.readLine();
                     System.out.println(name);
                     if (!name.isEmpty()) {
-                        data[1] = name;
+                        regg[1] = name;
                         break;
                     }
                 }
@@ -73,7 +72,7 @@ public class Server{
                     String l_name = br.readLine();
                     System.out.println(l_name);
                     if (!l_name.isEmpty()) {
-                        data[2] = l_name;
+                        regg[2] = l_name;
                         break;
                     }
                 }
@@ -82,7 +81,7 @@ public class Server{
                     String password = br.readLine();
                     System.out.println(password);
                     if (!password.isEmpty()) {
-                        data[3] = password;
+                        regg[3] = password;
                         break;
                     }
                 }
@@ -91,14 +90,48 @@ public class Server{
                     String role = br.readLine();
                     System.out.println(role);
                     if (!role.isEmpty()) {
-                        data[4] = role;
+                        regg[4] = role;
                         break;
                     }
                 }
-                Register(conn, data, ps);
-                //skorej sa bude robit registracia
-
-
+                Register(conn, regg, ps);
+            }
+            else if (line.equals("Login") || line.equals("login")){
+                while (true)
+                {
+                    ps.println("Please enter your name");
+                    String l_name = br.readLine();
+                    System.out.println("lid");
+                    if (!l_name.isEmpty()) {
+                        logg[0] = l_name;
+                        break;
+                    }
+                }
+                while (true)
+                {
+                    ps.println("Please enter you password");
+                    String lpassword = br.readLine();
+                    System.out.println("lid");
+                    if (!lpassword.isEmpty()) {
+                        logg[1] = lpassword;
+                        break;
+                    }
+                }
+               String[] check;
+                check = (String[]) Login(conn,logg);
+                /*if (check[0].isEmpty()) {
+                    ps.println(Arrays.toString(check));
+                }*/
+                if (check[2].equals("student"))
+                {
+                    login_student = 1;
+                    ps.println("Boli ste prihlaseny ako student");
+                }
+                else if(check[2].equals("teacher"))
+                {
+                    login_teacher = 1;
+                    ps.println("Boli ste prihlaseny ako ucitel");
+                }
             }
 
         }
@@ -110,15 +143,47 @@ public class Server{
         String lastname = more[2];
         String password = more[3];
         String role = more[4];
-        Statement stm = null;
+        Statement st = null;
         String SQL = "insert into register(id, name,lastname, password, role) VALUES ('" + ID + "', '" + name + "','" + lastname + "','" + password + "','" + role + "')";
         try {
-            stm = conn.createStatement();
+            st = conn.createStatement();
 
-            stm.executeUpdate(SQL);
+            st.executeUpdate(SQL);
         } catch (Exception e) {
             e.printStackTrace();
         }
         ps.println("Successfully written into DB!");
+    }
+
+    public static Serializable Login(Connection conn, String[] lmore) throws Exception{
+        String name = lmore[0];
+        String paswd = lmore[1];
+        String[] drive = new String[3];
+
+        String query = "SELECT * from register";
+
+        try
+        {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next())
+            {
+                int id = rs.getInt("id");
+                drive[0] = rs.getString("name");
+                drive[1] = rs.getString("password");
+                drive[2] = rs.getString("role");
+            }
+        } catch (Exception e) {System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());}
+
+        if (drive[0].equals(name) && drive[1].equals(paswd))
+        {
+            return drive;
+        }
+        else{
+            //System.out.println("Fuck off mate");
+            return "Nope";
+        }
     }
 }
