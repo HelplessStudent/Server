bd7afcad9834c69b01131dd473f9a3819e21d090
HelplessStudent <74906645+HelplessStudent@users.noreply.github.com>
2021-12-06 22:49:48 +0100

    3.0
    Almost done, only very small backend

--- src/Client.java
+++ src/Client.java
@@ -6,7 +6,7 @@ import java.net.*;
 
 class Client {
 
-    public static void main(String args[])
+    public static void main(String[] args)
             throws Exception
     {
 
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.nimbus.State;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -5,8 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.sql.*;
-import java.util.Arrays;
+
 
 public class Server{
     public static int ID = 0;
@@ -101,7 +101,7 @@ public class Server{
                 {
                     ps.println("Please enter your name");
                     String l_name = br.readLine();
-                    System.out.println("lid");
+                    System.out.println(l_name);
                     if (!l_name.isEmpty()) {
                         logg[0] = l_name;
                         break;
@@ -111,13 +111,13 @@ public class Server{
                 {
                     ps.println("Please enter you password");
                     String lpassword = br.readLine();
-                    System.out.println("lid");
+                    System.out.println(lpassword);
                     if (!lpassword.isEmpty()) {
                         logg[1] = lpassword;
                         break;
                     }
                 }
-               String[] check;
+                String[] check = new String[5];
                 check = (String[]) Login(conn,logg);
                 /*if (check[0].isEmpty()) {
                     ps.println(Arrays.toString(check));
@@ -125,7 +125,7 @@ public class Server{
                 if (check[2].equals("student"))
                 {
                     login_student = 1;
-                    ps.println("Boli ste prihlaseny ako student");
+                    ps.println("Boli ste prihlaseny ako student" + login_student);
                 }
                 else if(check[2].equals("teacher"))
                 {
@@ -133,7 +133,97 @@ public class Server{
                     ps.println("Boli ste prihlaseny ako ucitel");
                 }
             }
+            if (login_student == 1) {
+                //System.out.println("Error code here!");
+                //ps.println("What do u want to do? 1. Make a Schedule, 2. Load your schedule");
+                String student;
+                student = br.readLine();
+                //System.out.println("Message: " + student);
+                Integer[] load = new Integer[3];
+                String[] schedule;
+                schedule = new String[4];
+                if (student.equals("1")) //Make ur schedule and registration into it
+                {
+                    while (true) {
+                        ps.println("Enter your ID");
+                        String ID = br.readLine();
+                        System.out.println("Tu je ID: " + ID);
+                        if (!ID.isEmpty()) {
+                            schedule[0] = ID;
+                            break;
+                        }
+                    }
+                    while (true) {
+                        ps.println("Enter name of the teacher:");
+                        String t_name = br.readLine();
+                        System.out.println("Tu je meno ucitela" + t_name);
+                        if (!t_name.isEmpty()) {
+                            schedule[1] = t_name;
+                            break;
+                        }
+                    }
+                    while (true) {
+                        ps.println("Enter your day: pondelok, utorok, piatok");
+                        String day = br.readLine();
+                        System.out.println("Tu je den, ktory ste si vybrali" + day);
+                        if (!day.isEmpty()) {
+                            schedule[2] = day;
+                            break;
+                        }
+                    }
+                    while (true) {
+                        ps.println("Enter time: 10-12:00 (10 min max)");
+                        String time = br.readLine();
+                        System.out.println("Tu je vas cas" + time);
+                        if (!time.isEmpty()) {
+                            schedule[3] = time;
+                            break;
+                        }
+                    }
+                    MakeSchedule(conn, schedule, ps);
+                }
+                else if (student.equals("2")) {
+                    while (true) {
+                        ps.println("Enter ur ID under u created ur Schedule registration");
+                        Integer id = Integer.valueOf(br.readLine());
+                        System.out.println(id);
+                        load[0] = id;
+                        break;
+                    }
+                    String[] s_check = new String[5];
+                    s_check = (String[]) S_LoadSchedule(conn,load);
+                    System.out.println("Teacher's name: " + s_check[1] );
+                    System.out.println("Day: " + s_check[2] );
+                    System.out.println("Time: " + s_check[3] );
 
+                    ps.println("Teacher's name: " + s_check[1] );
+                    ps.println("\nDay: " + s_check[2] );
+                    ps.println("Time: " + s_check[3] );
+                }
+
+            }
+            if (login_teacher == 1){
+                String readl = br.readLine();
+                String[] t_schedule = new String [2];
+                if (readl.equals("1"))
+                {
+                    while (true){
+                        ps.println("Please enter your name: ");
+                        String t_name = br.readLine();
+                        if (!t_name.isEmpty()) {
+                            t_schedule[0] = t_name;
+                            break;
+                        }
+                    }
+                    String[] t_check = new String[50];
+                    t_check = (String[]) T_LoadSchedule(conn, t_schedule);
+
+                    for (int i = 0; i <= 10; i++){
+                        ps.println("Message: " + t_check[i]);
+                    }
+                }
+
+            }
         }
     }
     public static void Register(Connection conn, String[] more, PrintStream ps) throws Exception {
@@ -154,13 +244,13 @@ public class Server{
         }
         ps.println("Successfully written into DB!");
     }
-
+    //Fix, make to Login by ID, because we cannot have more people with same ID, but we can more people with same name
     public static Serializable Login(Connection conn, String[] lmore) throws Exception{
         String name = lmore[0];
         String paswd = lmore[1];
         String[] drive = new String[3];
 
-        String query = "SELECT * from register";
+        String query = "SELECT * from register where name ='"+name+"'";
 
         try
         {
@@ -177,7 +267,7 @@ public class Server{
         } catch (Exception e) {System.err.println("Got an exception! ");
             System.err.println(e.getMessage());}
 
-        if (drive[0].equals(name) && drive[1].equals(paswd))
+        if (drive[0] != null && drive[1].equals(paswd))
         {
             return drive;
         }
@@ -186,4 +276,82 @@ public class Server{
             return "Nope";
         }
     }
+
+    public static void MakeSchedule(Connection conn, String[] consulting, PrintStream ps) throws Exception{
+        String ID = consulting[0];
+        String name = consulting[1];
+        String day = consulting[2];
+        String time = consulting[3];
+        //Statement st = null;
+        String SQL = "insert into t_consultation(id, name, day ,time ) VALUES ('" + ID + "','"+ name +"','" + day + "','" + time + "')";
+        try {
+
+            Statement st = conn.createStatement();
+            st.executeUpdate(SQL);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ps.println("Boli ste pridany na cakaciu listinu");
+    }
+
+    public static Serializable S_LoadSchedule(Connection conn, Integer[] load) throws Exception {
+        Integer s_id = load[0]; //pripravil som si ID
+
+        String[] drive = new String[4];
+        //Integer[] drive1 = new Integer[3];
+
+        String query = "SELECT * from t_consultation where id ='"+s_id+"'";
+
+        try
+        {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next())
+            {
+                int id = rs.getInt("id");
+                drive[1] = rs.getString("name");
+                drive[2] = rs.getString("day");
+                drive[3] = rs.getString("time");
+            }
+        } catch (Exception e) {System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());}
+
+        if (( s_id != null))
+            return drive;
+        else{
+            //System.out.println("Fuck off mate");
+            return "Nope";
+        }
+    }
+
+    public static Serializable T_LoadSchedule(Connection conn, String[] load) throws IOException{
+        String name = load[0];
+        String[] drive = new String[50];
+
+        String query = "SELECT * from t_consultation where name ='"+name+"'";
+
+        try
+        {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            int up = 0;
+            while (rs.next())
+            {
+                int id = rs.getInt("id");
+                String s_name = rs.getString("name");
+                String s_day = rs.getString("day");
+                String s_time = rs.getString("time");
+                drive[up] =  ("id: "+id+"\n name: " + s_name+"\n day: "+s_day+" \n time: "+s_time);
+                up++;
+            }
+        } catch (Exception e) {System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());}
+        return drive;
+    }
+
 }
+
+
